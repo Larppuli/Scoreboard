177082d6e29f7a920b8b9aa02259ffa1d676b8c7
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-11-19 18:52:20 +0200

    added logo and changed font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import Leaguetable from './components/Leaguetable';
-import { Typography } from '@mui/material';
+import { Typography, Stack } from '@mui/material';
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -30,10 +30,13 @@ const App = () => {
     fetchData();
   }, []);
   return (
-    <div>
-      <Typography variant="h2" color='white'>
-        Poikainscore
-      </Typography>
+    <div align="center">
+      <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
+        <img src="/images/FlashscoreLogo.png" alt="Logo" style={{ width: '50px', height: 'auto' }} />
+        <Typography sx={{ fontFamily: '"Audiowide", sans-serif', fontSize: '35px' }} color='white'>
+          POIKAINSCORE
+        </Typography>
+      </Stack>
       <Leaguetable games={data}/>
     </div>
   );
