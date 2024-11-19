4173192f7d5562a902483e06f73e9dfa7b1a672f
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-11-19 18:53:28 +0200

    Initial commit

---
+++ src/components/Formbox.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+const Formbox = ({ game, player }) => {
+  const pickColor = () => {
+    let color;
+    let indicatorLetter;
+
+    if (game.winner === player) {
+      color = "#60a44c";
+      indicatorLetter = 'W';
+    } else {
+      color = "#c02c1c";
+      indicatorLetter = 'L';
+    }
+
+    return { color, indicatorLetter };
+  };
+
+  const { color, indicatorLetter } = pickColor();
+
+  return (
+    <Paper
+      sx={{
+        background: color,
+        color: 'white',
+        padding: '10px',
+        textAlign: 'center',
+        width: '2px',
+        height: '2px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      elevation={1}
+    >      <Typography variant="body1">{indicatorLetter}</Typography>
+    </Paper>
+  );
+};
+
+export default Formbox;
