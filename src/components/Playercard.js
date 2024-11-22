d8d7bdbf03362559e2cf531e0f73dc044dedef21
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-11-22 19:13:31 +0200

    implementation of market value chart

--- src/components/Playercard.js
+++ src/components/Playercard.js
@@ -3,29 +3,76 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import { Grow } from '@mui/material';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
-const Playercard = ({photoId, name, games}) => {
+ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+const Playercard = ({ photoId, name, games }) => {
     const firstName = name.split(' ')[0];
     const playerGames = games.filter((game) => game.participants.includes(firstName));
     const eightLatestGames = playerGames.slice(playerGames.length - 8, playerGames.length);
-    
-    const calculateMarketValue = () => {
+
+    const calculateMarketValueDevelopment = () => {
         let marketValue = 10;
-    
-        eightLatestGames.slice().reverse().forEach((game, index) => {
+        const marketValues = [];
+
+        eightLatestGames.forEach((game, index) => {
             const weight = Math.max(0.1, 3 - index * 0.1);
             const lossWeight = Math.max(0.1, 0.4 - index * 0.1);
-    
+
             if (game.winner === firstName) {
-                marketValue += 6 * weight;
+                marketValue += 3 * weight;
             } else {
                 marketValue -= 2 * lossWeight;
             }
+            marketValues.push(marketValue.toFixed(1));
         });
-    
-        return marketValue.toFixed(1);
+
+        return marketValues;
     };
-    
+
+    const marketValueDevelopment = calculateMarketValueDevelopment();
+
+    const chartData = {
+        labels: playerGames.slice(playerGames.length - 8, playerGames.length).map((_, index) => `Game ${index + 1}`),
+        datasets: [
+            {
+                data: marketValueDevelopment,
+                borderColor: 'rgba(75, 192, 192, 1)',
+            },
+        ],
+    };
+
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: (value) => `€${value}M`,
+                },
+            },
+            x: {
+                display: false
+            }
+        },
+    };
+
     return (
         <Grow in={true} timeout={500}>
             <Paper
@@ -36,9 +83,10 @@ const Playercard = ({photoId, name, games}) => {
                     marginTop: '10px',
                 }}
                 elevation={2}
-                align='left'
-                >
-                    <Stack direction="row"> 
+                align="left"
+            >
+                <Stack spacing={2}>
+                    <Stack direction="row">
                         <img
                             src={`https://drive.google.com/thumbnail?id=${photoId}`}
                             alt="Player Thumbnail"
@@ -46,18 +94,21 @@ const Playercard = ({photoId, name, games}) => {
                                 borderRadius: '6%',
                                 width: '60px',
                                 height: '60px',
-
                             }}
                         />
-                        <Stack paddingLeft={1} spacing={0.01}>
+                        <Stack paddingLeft={1} spacing={0.5}>
                             <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
                                 {name}
                             </Typography>
                             <Typography variant="body2">
-                                Market value: €{calculateMarketValue()}M
+                                Current Market Value: €{marketValueDevelopment[marketValueDevelopment.length - 1]}M
                             </Typography>
                         </Stack>
                     </Stack>
+                    <div style={{ height: '150px', width: '100%' }}>
+                        <Line data={chartData} options={chartOptions} />
+                    </div>
+                </Stack>
             </Paper>
         </Grow>
     );
