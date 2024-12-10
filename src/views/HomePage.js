0dd216dd0107043ed22c19acd4a348636416e91d
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-10 18:15:30 +0200

    moved the code from App.js to HomePage.js

---
+++ src/views/HomePage.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import Leaguetable from '../components/Leaguetable';
+import { Typography, Stack, Box } from '@mui/material';
+import Sportcard from '../components/Sportcard';
+import { v4 as uuidv4 } from 'uuid';
+import Playercard from '../components/Playercard';
+
+const HomePage = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const apiUrl = process.env.REACT_APP_API_URL;
+        const response = await fetch(apiUrl);
+        const csvText = await response.text();
+
+        Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+          complete: (result) => {
+            const games = result.data.map(row => ({
+              date: row['Pelipäivä'],
+              winner: row['Voittaja'],
+              participants: row['Osallistujat'],
+              sport: row['Laji'], 
+            }));
+            setData(games);
+          },
+        });
+      };
+
+      fetchData();
+    }, []);
+
+    const groupBySport = () => {
+      const grouped = {};
+      data.forEach(game => {
+        if (!grouped[game.sport]) {
+          grouped[game.sport] = [];
+        }
+        grouped[game.sport].push(game);
+      });
+      return grouped;
+    };
+
+    return (
+      <Box sx={{ paddingBottom: '80px' }} align='center'>
+          <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
+            <img src="/images/FlashscoreLogo.png" alt="Logo" style={{ width: '50px', height: 'auto' }} />
+            <Typography sx={{ fontFamily: '"Audiowide", sans-serif', fontSize: '35px' }} color='white'>
+              POIKAINSCORE
+            </Typography>
+          </Stack>
+          <Leaguetable games={data} />
+          <Playercard photoId={process.env.REACT_APP_OSKARI_ID} name='Oskari Valkama' games={data} />
+          <Playercard photoId={process.env.REACT_APP_JANNE_ID} name='Janne Peltokorpi' games={data} />
+          <Playercard photoId={process.env.REACT_APP_LAURI_ID} name='Lauri Talvitie' games={data} />
+          <Playercard photoId={process.env.REACT_APP_EERO_ID} name='Eero Reijonen' games={data} />
+          {Object.entries(groupBySport()).map((sportData) => {
+            return <Sportcard sportData={sportData} key={uuidv4()} />;
+          })}
+      </Box>
+    );
+};
+
+export default HomePage;
