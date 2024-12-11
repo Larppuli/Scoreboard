cb92e489c7f20ec61033ac3ba96a1fac0ca6fabd
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-11 12:42:22 +0200

    fixed alignment to center

--- src/views/AchievementsPage.js
+++ src/views/AchievementsPage.js
@@ -1,23 +1,23 @@
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import Achievementcard from '../components/Achievementcard';
 
 const AchievementPage = ({ data }) => {
     return (
-        <div>
+        <Box>
             <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
                 <img src="/images/FlashscoreLogo.png" alt="Logo" style={{ width: '50px', height: 'auto' }} />
                 <Typography sx={{ fontFamily: '"Audiowide", sans-serif', fontSize: '35px' }} color='white'>
                 POIKAINSCORE
                 </Typography>
             </Stack>
-            <Stack paddingBottom={10}>
+            <Stack paddingBottom={10} alignItems="center">
                 <Achievementcard  photoId={process.env.REACT_APP_OSKARI_ID} name='Oskari Valkama' games={data}/>
                 <Achievementcard  photoId={process.env.REACT_APP_JANNE_ID} name='Janne Peltokorpi' games={data}/>
                 <Achievementcard  photoId={process.env.REACT_APP_LAURI_ID} name='Lauri Talvitie' games={data}/>
                 <Achievementcard  photoId={process.env.REACT_APP_EERO_ID} name='Eero Reijonen' games={data}/>
             </Stack>
-        </div>
+        </Box>
     );
 };
 
