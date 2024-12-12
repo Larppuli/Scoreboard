17f1e43f31025879ee842ce33715fbc87137e307
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-12 13:43:56 +0200

    edited to support multiple StarIcons

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -8,7 +8,7 @@ import {
 } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 
-const Achievement = ({ achievement, unlocked }) => {
+const Achievement = ({ achievement, unlocked, stars }) => {
     return (
         <TableContainer>
             <Table sx={{ borderCollapse: 'collapse' }}>
@@ -25,15 +25,18 @@ const Achievement = ({ achievement, unlocked }) => {
                             {achievement}
                         </TableCell>
                         <TableCell sx={{ border: 'none' }}>
-                        <StarIcon
-                            sx={{
-                                color: unlocked ? 'gold' : '#3a3a3a',
-                                transition: 'color 0.2s ease-in-out',
-                                filter: unlocked
-                                ? 'drop-shadow(0 0 1.5px rgba(255, 215, 0, 0.8)) drop-shadow(0 0 3px rgba(255, 215, 0, 0.8))' 
-                                : 'none',
-                            }}
-                        />
+                            {Array.from({ length: stars }).map((_, index) => (
+                                <StarIcon
+                                    key={index}
+                                    sx={{
+                                        color: index < unlocked ? 'gold' : '#3a3a3a',
+                                        transition: 'color 0.2s ease-in-out',
+                                        filter: index < unlocked
+                                            ? 'drop-shadow(0 0 1.5px rgba(255, 215, 0, 0.8)) drop-shadow(0 0 3px rgba(255, 215, 0, 0.8))'
+                                            : 'none',
+                                    }}
+                                />
+                            ))}
                         </TableCell>
                     </TableRow>
                 </TableBody>
