341f6df04f7351d277a1b224c63190e4ff8f395d
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-14 19:03:47 +0200

    implementation of achievement name rendering

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {
+    Stack,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableRow,
+    Typography,
 } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 
-const Achievement = ({ achievement, unlocked, stars }) => {
+const Achievement = ({ achievement, unlocked, stars, expanded, name }) => {
     return (
         <TableContainer>
             <Table sx={{ borderCollapse: 'collapse' }}>
@@ -19,21 +21,34 @@ const Achievement = ({ achievement, unlocked, stars }) => {
                                 border: 'none',
                                 color: 'white',
                                 fontWeight: 'bold',
-                                width: 220,
+                                width: 200,
                             }}
                         >
-                            {achievement}
+                            <Stack>
+                                <Typography fontWeight={"bold"} fontSize={20}>
+                                    {name}
+                                </Typography>
+                                <Typography fontSize={14}>
+                                    {achievement}
+                                </Typography>
+                            </Stack>
                         </TableCell>
                         <TableCell sx={{ border: 'none' }}>
                             {Array.from({ length: stars }).map((_, index) => (
                                 <StarIcon
                                     key={index}
                                     sx={{
-                                        color: index < unlocked ? 'gold' : '#3a3a3a',
-                                        transition: 'color 0.2s ease-in-out',
-                                        filter: index < unlocked
+                                        color: (index < unlocked) && expanded ? 'gold' : '#3a3a3a',
+                                        transition: `color 0.5s ${1 + index * 0.7}s ease-in-out, filter 0.5s ${1 + index * 0.7}s ease-in-out`,
+                                        filter: (index < unlocked) && expanded
                                             ? 'drop-shadow(0 0 1.5px rgba(255, 215, 0, 0.8)) drop-shadow(0 0 3px rgba(255, 215, 0, 0.8))'
                                             : 'none',
+                                        animation: (index < unlocked) && expanded ? 'quick-spin 0.5s ease-in-out' : 'none',
+                                        animationDelay: `${1 + index * 0.7}s`,
+                                        '@keyframes quick-spin': {
+                                            '0%': { transform: 'rotate(0deg)' },
+                                            '100%': { transform: 'rotate(360deg)' },
+                                        },
                                     }}
                                 />
                             ))}
