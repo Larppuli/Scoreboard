16a263944e91bdd954f81491b2053feebb2bb759
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-21 01:42:10 +0200

    edited to have default values in GamesPage

--- src/components/MenuSelection.js
+++ src/components/MenuSelection.js
@@ -1,73 +1,73 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react';
-import { Select, MenuItem, FormControl, InputLabel, OutlinedInput, Checkbox, ListItemText } from '@mui/material';
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from 'react';
+import {
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    Checkbox,
+    ListItemText,
+} from '@mui/material';
 import WarningFilter from '../utils/WarningFilter';
 
-const MenuSelection = forwardRef(({ multi, selections, label, onSelectionChange }, ref) => {
-    const [selectedItems, setSelectedItems] = useState([]);
+const MenuSelection = forwardRef(
+    ({ multi, selections, label, onSelectionChange, customSx, color, defaultValue = [], placeholder, autoSelect = [] }, ref) => {
+        const [selectedItems, setSelectedItems] = useState(defaultValue);
 
-    const handleSelectionChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setSelectedItems(
-            typeof value === 'string' ? value.split(',') : value
+        const handleSelectionChange = (event) => {
+            const {
+                target: { value },
+            } = event;
+            setSelectedItems(
+                typeof value === 'string' ? value.split(',') : value
+            );
+            onSelectionChange(value);
+        };
+
+        useEffect(() => {
+            if (autoSelect.length > 0) {
+                setSelectedItems(autoSelect);
+            }
+        }, [autoSelect]);
+
+        useImperativeHandle(ref, () => ({
+            handleClearSelection: () => {
+                setSelectedItems([]);
+            },
+            setSelections: (newSelections) => {
+                setSelectedItems(newSelections);
+            },
+        }));
+
+        return (
+            <WarningFilter>
+                <FormControl sx={{ marginTop: '20px' }}>
+                    <InputLabel id="demo-multiple-checkbox-label" sx={{ color: color }}>
+                        {label}
+                    </InputLabel>
+                    <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple={multi}
+                        value={selectedItems}
+                        onChange={handleSelectionChange}
+                        input={<OutlinedInput label={label} />}
+                        renderValue={(selected) =>
+                            selected.length > 0 ? selected.join(', ') : <span style={{ color: '#9e9e9e' }}>{placeholder}</span>
+                        }
+                        sx={customSx}
+                    >
+                        {selections.map((item, index) => (
+                            <MenuItem key={index} value={item}>
+                                <Checkbox checked={selectedItems.includes(item)} />
+                                <ListItemText primary={item} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </WarningFilter>
         );
-        onSelectionChange(value);
-    };
-
-    useImperativeHandle(ref, () => ({
-        handleClearSelection: () => {
-            setSelectedItems([]);
-        }
-    }));
-
-    return (
-        <WarningFilter>
-            <FormControl sx={{ marginTop: '20px' }}>
-                <InputLabel id="demo-multiple-checkbox-label" sx={{ color: 'white' }}>
-                    {label}
-                </InputLabel>
-                <Select
-                    labelId="demo-multiple-checkbox-label"
-                    id="demo-multiple-checkbox"
-                    multiple={multi}
-                    value={selectedItems}
-                    onChange={handleSelectionChange}
-                    input={<OutlinedInput label={label} />}
-                    renderValue={(selected) => selected.join(', ')}
-                    sx={{
-                        '& .MuiInputLabel-root': {
-                            color: 'white',
-                        },
-                        '& .MuiInputBase-root': {
-                            color: 'white',
-                        },
-                        '& .MuiSvgIcon-root': {
-                            color: 'white',
-                        },
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: 'white',
-                            },
-                        },
-                        '& input::placeholder': {
-                            color: 'white',
-                        },
-                        background: '#080c0c',
-                        borderRadius: '3px',
-                        color: 'white',
-                    }}
-                >
-                    {selections.map((item, index) => (
-                        <MenuItem key={index} value={item}>
-                            <Checkbox checked={selectedItems.includes(item)} />
-                            <ListItemText primary={item} />
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-        </WarningFilter>
-    );
-});
+    }
+);
 
 export default MenuSelection;
