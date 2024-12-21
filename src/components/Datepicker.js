29602a1dbb6861a630677e3a5599a07b712a4813
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-12-21 02:06:22 +0200

    fixed bug of selecting one day before actual date

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 
 const Datepicker = ({ defaultDate, customSx, selectedDate, setSelectedDate }) => {
-
     useEffect(() => {
         if (defaultDate) {
-            setSelectedDate(dayjs(defaultDate, 'DD.MM.YYYY'));
+            setSelectedDate(dayjs(defaultDate, 'DD.MM.YYYY').endOf('day'));
         }
     }, [defaultDate, setSelectedDate]);
 
-    const handleDateChange = (newDate) => {
-        setSelectedDate(newDate);
+    const handleDateChange = (date) => {
+        // Ensure it's normalized
+        const normalizedDate = dayjs(date).endOf('day');
+        setSelectedDate(normalizedDate);
     };
 
     return (
