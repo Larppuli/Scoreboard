18ca6443b25508db24ced95d738844f637951a65
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-07 01:10:02 +0200

    added caching

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registered with scope: ', registration.scope);
+
+      if (registration.waiting) {
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      }
+    }).catch((error) => {
+      console.log('Service Worker registration failed: ', error);
+    });
+  });
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
