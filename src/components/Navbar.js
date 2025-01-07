5ac51140864d41e789cf18e9ddff83a7d9a1dc52
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-07 16:19:02 +0200

    edited navbar height and added react-router-dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import EqualizerIcon from '@mui/icons-material/Equalizer';
 import { BottomNavigationAction, BottomNavigation } from '@mui/material';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import SportsKabaddiIcon from '@mui/icons-material/SportsKabaddi';
-import { useLocation } from 'react-router';
 import ListIcon from '@mui/icons-material/List';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
     const location = useLocation();
@@ -20,8 +20,7 @@ const Navbar = () => {
                 backdropFilter: 'blur(5px)',
                 bottom: 0,
                 top: 'auto',
-                height: '100px',
-                justifyContent: 'center',
+                height: '90px',
             }}
         >
             <Toolbar>
@@ -33,47 +32,53 @@ const Navbar = () => {
                         marginBottom: '8px'
                     }}
                 >
-                <BottomNavigationAction
-                    href="/"
-                    icon={<EqualizerIcon sx={{ color: currentPath === '/' ? 'white' : '#969696' }} />}
-                    label="Stats"
-                    style={{
-                        color: currentPath === '/' ? 'white' : '#969696',
-                        background: currentPath === '/' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
-                        borderRadius: '5px'
-                    }}
-                />
+                    <BottomNavigationAction
+                        component={Link}
+                        to="/"
+                        icon={<EqualizerIcon sx={{ color: currentPath === '/' ? 'white' : '#969696' }} />}
+                        label="Stats"
+                        style={{
+                            color: currentPath === '/' ? 'white' : '#969696',
+                            background: currentPath === '/' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
+                            borderRadius: '5px',
+                        }}
+                    />
 
-                <BottomNavigationAction
-                    href="/achievements"
-                    icon={<EmojiEventsIcon sx={{ color: currentPath === '/achievements' ? 'white' : '#969696' }} />}
-                    label="Achievements"
-                    style={{
-                        color: currentPath === '/achievements' ? 'white' : '#969696',
-                        background: currentPath === '/achievements' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
-                        borderRadius: '5px'
-                    }}
-                />
-                <BottomNavigationAction
-                    href="/new-game"
-                    icon={<SportsKabaddiIcon sx={{ color: currentPath === '/new-game' ? 'white' : '#969696' }} />}
-                    label="New game"
-                    style={{
-                        color: currentPath === '/new-game' ? 'white' : '#969696',
-                        background: currentPath === '/new-game' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
-                        borderRadius: '5px'
-                    }}
-                />
-                <BottomNavigationAction
-                    href="/games"
-                    icon={<ListIcon sx={{ color: currentPath === '/games' ? 'white' : '#969696' }} />}
-                    label="Games"
-                    style={{
-                        color: currentPath === '/games' ? 'white' : '#969696',
-                        background: currentPath === '/games' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
-                        borderRadius: '5px'
-                    }}
-                />
+                    <BottomNavigationAction
+                        component={Link}
+                        to="/achievements"
+                        icon={<EmojiEventsIcon sx={{ color: currentPath === '/achievements' ? 'white' : '#969696' }} />}
+                        label="Achievements"
+                        style={{
+                            color: currentPath === '/achievements' ? 'white' : '#969696',
+                            background: currentPath === '/achievements' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
+                            borderRadius: '5px',
+                        }}
+                    />
+
+                    <BottomNavigationAction
+                        component={Link}
+                        to="/new-game"
+                        icon={<SportsKabaddiIcon sx={{ color: currentPath === '/new-game' ? 'white' : '#969696' }} />}
+                        label="New Game"
+                        style={{
+                            color: currentPath === '/new-game' ? 'white' : '#969696',
+                            background: currentPath === '/new-game' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
+                            borderRadius: '5px',
+                        }}
+                    />
+
+                    <BottomNavigationAction
+                        component={Link}
+                        to="/games"
+                        icon={<ListIcon sx={{ color: currentPath === '/games' ? 'white' : '#969696' }} />}
+                        label="Games"
+                        style={{
+                            color: currentPath === '/games' ? 'white' : '#969696',
+                            background: currentPath === '/games' ? 'rgba(40, 40, 40, 0.3)' : 'inherit',
+                            borderRadius: '5px',
+                        }}
+                    />
                 </BottomNavigation>
             </Toolbar>
         </AppBar>
