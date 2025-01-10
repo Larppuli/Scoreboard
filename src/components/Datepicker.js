1762645d27f6638e33bac4280961a33659a09693
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-10 15:13:54 +0200

    implemented data formatting when component is closed

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -1,34 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, forwardRef, useImperativeHandle } from 'react';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+
 dayjs.extend(utc);
 
-const Datepicker = ({ defaultDate, customSx, selectedDate, setSelectedDate }) => {
-    useEffect(() => {
-        if (defaultDate) {
-            setSelectedDate(dayjs(defaultDate, 'DD.MM.YYYY').endOf('day'));
-        }
-    }, [defaultDate, setSelectedDate]);
+const Datepicker = forwardRef(
+    ({ defaultDate, customSx, selectedDate, setSelectedDate }, ref) => {
+        useEffect(() => {
+            if (defaultDate) {
+                setSelectedDate(dayjs(defaultDate, 'DD.MM.YYYY').endOf('day'));
+            }
 
-    const handleDateChange = (date) => {
-        // Ensure it's normalized
-        const normalizedDate = dayjs(date).endOf('day');
-        setSelectedDate(normalizedDate);
-    };
+            return () => {
+                setSelectedDate(null);
+            };
+        }, [defaultDate, setSelectedDate]);
 
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-                label="Select Date"
-                value={selectedDate}
-                onChange={handleDateChange}
-                format="DD.MM.YYYY"
-                sx={customSx}
-            />
-        </LocalizationProvider>
-    );
-};
+        const handleDateChange = (date) => {
+            const normalizedDate = date ? dayjs(date).endOf('day') : null;
+            setSelectedDate(normalizedDate);
+        };
+
+        useImperativeHandle(ref, () => ({
+            handleClearSelection: () => {
+                setSelectedDate(null);
+            },
+            setSelection: (newDate) => {
+                const normalizedDate = dayjs(newDate, 'DD.MM.YYYY').endOf('day');
+                setSelectedDate(normalizedDate);
+            },
+        }));
+
+        return (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label="Select Date"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    sx={customSx}
+                    format="DD.MM.YYYY"
+                />
+            </LocalizationProvider>
+        );
+    }
+);
 
 export default Datepicker;
