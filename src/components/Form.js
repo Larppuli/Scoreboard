f8247cec9792d94f82a0232a48c4291f8114be27
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-13 14:32:50 +0200

    edited to contain 4 form boxes

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,19 @@ import { Stack } from '@mui/material';
 import Formbox from './Formbox';
 
 const Form = ({ games, player }) => {
-  const [lastFiveGames, setLastFiveGames] = useState([{},{},{},{},{}])
+  const [lastFourGames, setLastFourGames] = useState([{},{},{},{}])
   useEffect(() => {
     if (games.length > 0) {
-      const slicedGames = games.slice(-5).reverse();
-      const emptySlots = 5 - slicedGames.length;
+      const slicedGames = games.slice(-4).reverse();
+      const emptySlots = 4 - slicedGames.length;
       const updatedGames = [...slicedGames, ...Array(Math.max(emptySlots, 0)).fill({})];
-      setLastFiveGames(updatedGames);
+      setLastFourGames(updatedGames);
     }
   }, [games]);
 
   return (
     <Stack direction="row" spacing={1} justifyContent="right">
-      {lastFiveGames.map((game, index) => {
+      {lastFourGames.map((game, index) => {
         return <Formbox game={game} player={player} key={player + index} />;
       })}
     </Stack>
