d3d9a31c7734dfd9b4930fc646537364c727f019
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-19 22:42:43 +0200

    removed color change

--- src/components/NumAnimation.js
+++ src/components/NumAnimation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from '@react-spring/web';
 
-const NumAnimation = ({ targetNumber, fixedNum, colorChange, fontWeight }) => {
+const NumAnimation = ({ targetNumber, fixedNum, fontWeight }) => {
   const validTargetNumber = isNaN(targetNumber) ? 0 : parseFloat(targetNumber);
 
   const { number } = useSpring({
@@ -10,33 +10,10 @@ const NumAnimation = ({ targetNumber, fixedNum, colorChange, fontWeight }) => {
     config: { mass: 1, tension: 20, friction: 10 },
   });
 
-  const calculateColor = (value) => {
-    if (colorChange) {
-        const adjustedValue = value - 15;
-        if (adjustedValue <= 20) {
-            const t = Math.max(adjustedValue / 20, 0);
-            return `rgb(255, ${Math.round(255 * t)}, 0)`;
-        } else if (adjustedValue <= 40) {
-            const t = (adjustedValue - 20) / 20;
-            return `rgb(${Math.round(255 - 255 * t)}, 255, 0)`;
-        } else if (adjustedValue <= 60) {
-            const t = (adjustedValue - 40) / 20;
-            return `rgb(0, 255, ${Math.round(255 * t)})`;
-        } else {
-            return `rgb(0, 255, 255)`;
-        }
-    } else {
-        return 'white';
-    }
-};
-
-
-
-
   return (
     <animated.span
       style={{
-        color: number.to((n) => calculateColor(n)),
+        color: 'white',
         fontWeight: fontWeight || '400'
       }}
     >
