5d71816051bcbe909600b1443a4c57742b7e8956
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-01-20 13:43:38 +0200

    edited pointing system

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = ({ data }) => {
     const uniqueParticipants = [...new Set(games.flatMap(game => game.participants))];
 
     const scoreObjects = uniqueParticipants.map((player) => {
-        return { player: player, scores: [0], games: 0, wins: 0, winWeights: 0 };
+        return { player: player, scores: [0], games: 0, wins: 0, gamePointWeights: 0 };
     });
 
     games.forEach(game => {
@@ -19,17 +19,18 @@ const HomePage = ({ data }) => {
             if (game.winner === player) {
                 scoreObject.wins += 1;
                 scoreObject.games += 1;
-                scoreObject.winWeights += game.participants.length;
+                scoreObject.gamePointWeights += game.participants.length;
             } else if (game.participants.includes(player)) {
                 scoreObject.games += 1;
+                scoreObject.gamePointWeights -= (5- game.participants.length);
             };
             if (scoreObject.games < 1) {
-              scoreObject.scores.push(scoreObject.wins / scoreObject.games * 50 + scoreObject.winWeights);
+              scoreObject.scores.push(Math.max(0, scoreObject.wins / scoreObject.games * 50 + scoreObject.gamePointWeights));
             } else if (scoreObject.games < 2) {
-              scoreObject.scores.push(scoreObject.wins / scoreObject.games * 70 + scoreObject.winWeights);
+              scoreObject.scores.push(Math.max(0, scoreObject.wins / scoreObject.games * 70 + scoreObject.gamePointWeights));
             } 
             else {
-              scoreObject.scores.push(scoreObject.wins / scoreObject.games * 200 + scoreObject.winWeights);
+              scoreObject.scores.push(Math.max(0, scoreObject.wins / scoreObject.games * 200 + scoreObject.gamePointWeights));
             };
         });
     });
